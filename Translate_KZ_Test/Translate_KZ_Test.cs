4cb307625d5768100a982cdafafc9a0d1e7cf8d1
savin.arseni <savin.arseni@live.ru>
2018-07-17 00:00:16 +0600

    Add new char into dict. Add new test.

--- Translate_KZ_Test/Translate_KZ_Test.cs
+++ Translate_KZ_Test/Translate_KZ_Test.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Translate_KZ;
 
@@ -7,12 +8,12 @@ namespace Translate_KZ_Test
     [TestClass]
     public class Translate_KZ_Test
     {
-        string parhForDict = @"C:\Projects\GitHubRepos\Qazaq-Transliteration\Dictionary"; // \Dictionary
+        string _pathForDict = Directory.GetCurrentDirectory();
 
         [TestMethod]
         public void GetDictionary_CyrillicQazaq_OnKaz()
         {
-            var test = new TranslateKz(parhForDict, "CyrillicQazaq");
+            var test = new TranslateKz(_pathForDict, "CyrillicQazaq");
 
             var res = test.ConvertTo("НҰРЛЫ ЖОЛ - БОЛАШАҚҚА БАСТАР ЖОЛ", DirectionType.OnKaz);
 
@@ -22,18 +23,28 @@ namespace Translate_KZ_Test
         [TestMethod]
         public void GetDictionary_QazaqAqparat_OnKaz()
         {
-            var test = new TranslateKz(parhForDict, "QazaqAqparat");
+            var test = new TranslateKz(_pathForDict, "QazaqAqparat");
 
             var res = test.ConvertTo("НҰРЛЫ ЖОЛ - БОЛАШАҚҚА БАСТАР ЖОЛ", DirectionType.OnKaz);
 
             Assert.AreEqual(res, "NURLİ JOL - BOLAŞAQQA BASTAR JOL");
         }
 
+        [TestMethod]
+        public void GetDictionary_GOST569_OnKaz()
+        {
+            var test = new TranslateKz(_pathForDict, "GOST-569");
+
+            var res = test.ConvertTo("НҰРЛЫ ЖОЛ - БОЛАШАҚҚА БАСТАР ЖОЛ", DirectionType.OnKaz);
+
+            Assert.AreEqual(res, "NURLY JOL - BOLAShAQQA BASTAR JOL");
+        }
+
 
         [TestMethod]
         public void GetDictionary_SwitchDict_OnKaz()
         {
-            var test = new TranslateKz(parhForDict, "CyrillicQazaq");
+            var test = new TranslateKz(_pathForDict, "CyrillicQazaq");
 
             var res = test.ConvertTo("НҰРЛЫ ЖОЛ - БОЛАШАҚҚА БАСТАР ЖОЛ", DirectionType.OnKaz);
 
@@ -50,7 +61,7 @@ namespace Translate_KZ_Test
         [TestMethod]
         public void GetDictionary_CyrillicQazaq_OutKaz()
         {
-            var test = new TranslateKz(parhForDict, "CyrillicQazaq");
+            var test = new TranslateKz(_pathForDict, "CyrillicQazaq");
 
             var res = test.ConvertTo("НЎРЛЫ ЖОЛ - БОЛАШАЌЌА БАСТАР ЖОЛ", DirectionType.OutKaz);
 
@@ -60,7 +71,7 @@ namespace Translate_KZ_Test
         [TestMethod]
         public void GetDictionary_QazaqAqparat_OutKaz()
         {
-            var test = new TranslateKz(parhForDict, "QazaqAqparat");
+            var test = new TranslateKz(_pathForDict, "QazaqAqparat");
 
             var res = test.ConvertTo("NURLİ JOL - BOLAŞAQQA BASTAR JOL", DirectionType.OutKaz);
 
@@ -70,7 +81,7 @@ namespace Translate_KZ_Test
         [TestMethod]
         public void GetDictionary_SwitchDict_OutKaz()
         {
-            var test = new TranslateKz(parhForDict, "CyrillicQazaq");
+            var test = new TranslateKz(_pathForDict, "CyrillicQazaq");
 
             var res = test.ConvertTo("НЎРЛЫ ЖОЛ - БОЛАШАЌЌА БАСТАР ЖОЛ", DirectionType.OutKaz);
 
@@ -85,17 +96,27 @@ namespace Translate_KZ_Test
         }
 
         [TestMethod]
-        public void GetDictionary_BigText_OnKaz()
+        public void GetDictionary_GOST_569_OnKaz()
         {
-            var test = new TranslateKz(parhForDict, "CyrillicQazaq");
+            var test = new TranslateKz(_pathForDict, "GOST-569");
 
-            string text = @"Ішкі диаметрі 40 мм, қабырғы қалыңдығы 4 мм жіксіз болат құбыр.
-                            Шаруашылық - ауыз суы, кәріздік және жылыту жүйелеріне тағайындауға арналған.
-                            Tапсырыс берушінің талаптары - бір топтағы құбрларды жеткізгенде құбырлардың ішкі жжәне сыртқы беті тегіс болуы керек.
-                            Құбырладың сыртқы, ішкі және тік қойылған кесіндісінің бетінде үлкейту құралдарының көмегінсз көзге түсетін копіршіктер, 
-                            саңылаулар, қуыстар, басқа да қосындылардың болу рұқсат етілмейді.";
+            string text = @"Аа Әә Бб Вв Гг Ғғ Дд Ее Ёё Жж Зз Ии Йй Кк Ққ Лл Мм Нн Ңң Оо Өө Пп Рр Сс Тт Уу Ұұ Үү Фф Хх Һһ Цц Чч Шш Щщ Ъъ Ыы Іі Ьь Ээ Юю Яя";
 
             var res = test.ConvertTo(text, DirectionType.OnKaz);
+
+            Assert.AreEqual(res, @"Aa Áá Bb Vv Gg Ǵǵ Dd Ee  Jj Zz Iı Iı Kk Qq Ll Mm Nn Ńń Oo Óó Pp Rr Ss Tt Ýý Uu Úú Ff Hh Hh  Chch Shsh   Yy Ii    ");
+        }
+
+        [TestMethod]
+        public void GetDictionary_BigText_OnKaz()
+        {
+            var test = new TranslateKz(_pathForDict, "GOST-569");
+
+            string text = @"Ішкі диаметрі 40 мм, қабырғы қалыңдығы 4 мм жіксіз болат құбыр. Шаруашылық - ауыз суы, кәріздік және жылыту жүйелеріне тағайындауға арналған. Tапсырыс берушінің талаптары - бір топтағы құбрларды жеткізгенде құбырлардың ішкі жжәне сыртқы беті тегіс болуы керек. Құбырладың сыртқы, ішкі және тік қойылған кесіндісінің бетінде үлкейту құралдарының көмегінсз көзге түсетін копіршіктер, саңылаулар, қуыстар, басқа да қосындылардың болу рұқсат етілмейді. Эляим.";
+
+            var res = test.ConvertTo(text, DirectionType.OnKaz);
+
+            Assert.AreEqual(res, @"Ishki diametri 40 mm, qabyrǵy qalyńdyǵy 4 mm jiksiz bolat qubyr. Sharýashylyq - aýyz sýy, kárizdik jáne jylytý júıelerine taǵaıyndaýǵa arnalǵan. Tapsyrys berýshiniń talaptary - bir toptaǵy qubrlardy jetkizgende qubyrlardyń ishki jjáne syrtqy beti tegis bolýy kerek. Qubyrladyń syrtqy, ishki jáne tik qoıylǵan kesindisiniń betinde úlkeıtý quraldarynyń kómeginsz kózge túsetin kopirshikter, sańylaýlar, qýystar, basqa da qosyndylardyń bolý ruqsat etilmeıdi. lim.");
         }
     }
 }
